6d3a7843aa27c1f0f746d53a4dad41aaa43afbec
EvilSick <masoud.mp78@gmail.com>
2023-12-24 16:38:12 +0330

    Add requirements.txt

--- drf/api/views.py
+++ drf/api/views.py
@@ -6,13 +6,13 @@ from blog.models import Article
 # from rest_framework.response import Response
 # from rest_framework.views import APIView
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListAPIView, ListCreateAPIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
 from rest_framework.authentication import BasicAuthentication
 from .permissions import IsSuperUser, IsSuperUSerOrReadOnly, IsAuthorOrReadOnly
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-
 
 
 class ArticleApiView(ListCreateAPIView):
@@ -25,16 +25,17 @@ class ArticleDetailApiView(RetrieveUpdateDestroyAPIView):
     serializer_class = ArticleSerializers
     permission_classes = [IsAuthorOrReadOnly]
 
+
 class UsersApiView(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UsersSerializers
     permission_classes = [IsAdminUser]
-    
+
+
 class UsersDetailApiView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UsersSerializers
     permission_classes = [IsAdminUser]
-
 
 # class RevokeToken(APIView):
 #     def delete(self, request):
